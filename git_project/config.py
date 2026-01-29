e011c57f8a1d1654e965f36426b3f519c977629f
kmhhosea <kmh.hosea@gmail.com>
2026-01-29 11:57:40 +0300

    Add files via upload

---
+++ git_project/config.py
@@ -0,0 +1,15 @@
+print("version 3 feature 2 added and the job is done")
+a = 20
+b = 50
+c = a + b
+print("sum =",c)
+
+x = 0
+y = 0
+def sum(x,y):
+	return (x + y)
+print(sum(4,68))
+print("changed")
+print("feature 1")
+print("feature 2")
+print("The job is done")
---
+++ git_project/src/index.py
@@ -0,0 +1,23 @@
+print("Version 3 feature 2 added")
+import math
+for i in range (12):
+	print("i =",i)
+if i = 5:
+	break
+num = 20
+while num <= 20:
+	print num
+	num -= num
+print("changed")
+def calVolume(h, l, w):
+	h = float(input("Give the height: "))
+	l = float(input("Give the lenght: "))
+	w = float(input("Give the width: "))
+	return (w*l + w*h + l*h)
+print(calVolume(h, l, w))
+
+
+def calArea(a,b):
+	return a*b
+print(calArea(3,45))
+print(math.sqrt(25))
